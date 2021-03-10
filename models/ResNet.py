6978447112f2428f55c247aaa5bd4fe84072b44e
Dong-Hee Kim <queez0405@gmail.com>
2021-03-10 14:19:15 +0900

    add selecting activation functions

--- models/ResNet.py
+++ models/ResNet.py
@@ -13,13 +13,14 @@ collecting = False
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, act_func, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
         self.shortcut = nn.Sequential()
+        self.act_func = act_func
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
@@ -30,7 +31,7 @@ class BasicBlock(nn.Module):
         out = self.conv1(x)
         if collecting:
             gram_feats.append(out)
-        out = F.relu(self.bn1(out))
+        out = self.act_func(self.bn1(out))
         if collecting:
             gram_feats.append(out)
         out = self.conv2(out)
@@ -42,7 +43,7 @@ class BasicBlock(nn.Module):
         out += self.shortcut(x)
         if collecting:
             gram_feats.append(self.shortcut(x))
-        out = F.relu(out)
+        out = self.act_func(out)
         if collecting:
             gram_feats.append(out)
         return out
@@ -50,7 +51,7 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, act_func, stride=1):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -58,8 +59,8 @@ class Bottleneck(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
         self.shortcut = nn.Sequential()
+        self.act_func = act_func
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
@@ -70,13 +71,13 @@ class Bottleneck(nn.Module):
         out = self.conv1(x)
         if collecting:
             gram_feats.append(out)
-        out = F.relu(self.bn1(out))
+        out = self.act_func(self.bn1(out))
         if collecting:
             gram_feats.append(out)
         out = self.conv2(out)
         if collecting:
             gram_feats.append(out)
-        out = F.relu(self.bn2(out))
+        out = self.act_func(self.bn2(out))
         if collecting:
             gram_feats.append(out)
         out = self.conv3(out)
@@ -88,33 +89,42 @@ class Bottleneck(nn.Module):
         out += self.shortcut(x)
         if collecting:
             gram_feats.append(self.shortcut(x))
-        out = F.relu(out)
+        out = self.act_func(out)
         if collecting:
             gram_feats.append(out)
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, act_func='relu'):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
+        if act_func == 'relu':
+            self.act_func = nn.ReLU()
+        elif act_func == 'gelu':
+            self.act_func = nn.GELU()
+        elif act_func == 'leaky_relu':
+            self.act_func = nn.LeakyReLU()
+        elif act_func == 'softplus':
+            self.act_func = nn.Softplus()
+
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, act_func=self.act_func)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, act_func=self.act_func)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, act_func=self.act_func)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, act_func=self.act_func)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, act_func):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, act_func, stride))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.act_func(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -127,7 +137,7 @@ class ResNet(nn.Module):
     # function to extact the multiple features
     def feature_list(self, x):
         out_list = []
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.act_func(self.bn1(self.conv1(x)))
         out_list.append(out)
         out = self.layer1(out)
         out_list.append(out)
@@ -144,7 +154,7 @@ class ResNet(nn.Module):
     
     # function to extact a specific feature
     def intermediate_forward(self, x, layer_index):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.act_func(self.bn1(self.conv1(x)))
         if layer_index == 1:
             out = self.layer1(out)
         elif layer_index == 2:
@@ -163,7 +173,7 @@ class ResNet(nn.Module):
 
     # function to extact the penultimate features
     def penultimate_forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.act_func(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -222,8 +232,8 @@ class ResNet(nn.Module):
                 for p,P in enumerate(power):
                     g_p = G_p(feat_L,P)
                     
-                    dev +=  (F.relu(mins[L][p]-g_p)/torch.abs(mins[L][p]+10**-6)).sum(dim=1,keepdim=True)
-                    dev +=  (F.relu(g_p-maxs[L][p])/torch.abs(maxs[L][p]+10**-6)).sum(dim=1,keepdim=True)
+                    dev +=  (self.act_func(mins[L][p]-g_p)/torch.abs(mins[L][p]+10**-6)).sum(dim=1,keepdim=True)
+                    dev +=  (self.act_func(g_p-maxs[L][p])/torch.abs(maxs[L][p]+10**-6)).sum(dim=1,keepdim=True)
                 batch_deviations.append(dev.cpu().detach().numpy())
             batch_deviations = np.concatenate(batch_deviations,axis=1)
             deviations.append(batch_deviations)
@@ -232,16 +242,16 @@ class ResNet(nn.Module):
         return deviations
 
 def ResNet18(args):
-    return ResNet(BasicBlock, [2,2,2,2], args.num_classes)
+    return ResNet(BasicBlock, [2,2,2,2], args.num_classes, args.act_func)
 
 def ResNet34(args):
-    return ResNet(BasicBlock, [3,4,6,3], args.num_classes)
+    return ResNet(BasicBlock, [3,4,6,3], args.num_classes, args.act_func)
 
 def ResNet50(args):
-    return ResNet(Bottleneck, [3,4,6,3], args.num_classes)
+    return ResNet(Bottleneck, [3,4,6,3], args.num_classes, args.act_func)
 
 def ResNet101(args):
-    return ResNet(Bottleneck, [3,4,23,3], args.num_classes)
+    return ResNet(Bottleneck, [3,4,23,3], args.num_classes, args.act_func)
 
 def ResNet152(args):
-    return ResNet(Bottleneck, [3,8,36,3], args.num_classes)
+    return ResNet(Bottleneck, [3,8,36,3], args.num_classes, args.act_func)
