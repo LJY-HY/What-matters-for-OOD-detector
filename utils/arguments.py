de31db6426ba73812693fd9d245a4df24c35d3c3
LJY-HY <v2fds@naver.com>
2021-03-17 22:05:30 +0900

    add distillation

--- utils/arguments.py
+++ utils/arguments.py
@@ -84,3 +84,25 @@ def get_Gram_detector_arguments():
     parser.add_argument('--outf', default='./workspace/output/', help='folder to output results')
     args = parser.parse_args()
     return args
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description = 'Knowledge Distillation')
+    parser.add_argument('--gpu_id', default='0', type=int, help='id(s) for CUDA_VISIBLE_DEVICES')
+    parser.add_argument('--in_dataset', default='cifar10', type=str, choices=['cifar10', 'cifar100', 'SVHN', 'imagenet'], help = 'dataset choice')
+    parser.add_argument('--arch', default = 'ResNet18', type=str, choices = ['MobileNet','DenseNet','ResNet18','ResNet34','ResNet50','ResNet101','WideResNet28_2','WideResNet28_10','WideResNet40_2','WideResNet40_4','EfficientNet'])
+    parser.add_argument('--optimizer', default = 'SGD', type=str, choices = ['SGD','Nesterov','Adam','AdamW'])
+    parser.add_argument('--lr','--learning-rate', default = 0.1, type=float, choices = [1.0,0.1,0.001,0.0005,0.0002,0.0001])
+    parser.add_argument('--epoch', default=300, type=int, help='number of total epochs')
+    parser.add_argument('--batch_size', default=128, type=int, choices=[64,128,256,512])
+    parser.add_argument('--dropout_rate', default=0, type=float, choices=[0,0.3,0.5,0.7])
+    parser.add_argument('--scheduler', default='MultiStepLR', type=str, choices=['MultiStepLR','CosineAnnealing','CosineWarmup'])
+    parser.add_argument('--tuning',action='store_true')
+    parser.add_argument('--wd', '--weight_decay','--wdecay', default=5e-4, type=float, choices=[5e-4,1e-2,1e-3,1e-4,1e-6])
+    parser.add_argument('--warmup_duration', default = 10, help = 'duration of warming up')
+    parser.add_argument('--BN','--batch_normalization',action='store_false')
+    parser.add_argument('--act_func', default='relu', type=str, choices=['relu','gelu','silu','leaky_relu','softplus'])
+    parser.add_argument('--alpha', default=0.5, type=float)
+    parser.add_argument('--temp', default=30, type=float, help='weight decay')
+    parser.add_argument('--trial', default = '0', type=str)
+    args = parser.parse_args()
+    return args    
